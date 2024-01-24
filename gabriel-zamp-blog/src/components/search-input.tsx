ec78a707ad4ad40818a1d3b2692aceed089ed351
gabrielzamp <gzampierimkt@gmail.com>
2024-01-24 15:36:49 -0300

    v1.1 - added clean search feature. it is still missing reponsiviness optimization

--- gabriel-zamp-blog/src/components/search-input.tsx
+++ gabriel-zamp-blog/src/components/search-input.tsx
@@ -7,12 +7,27 @@ export const PostsSearchBar = () => {
   const { setPosts, posts } = usePosts();
   const [searchInput, setSearchInput] = useState("");
 
+  const handleCleanSearch = async (evt) => {
+    evt.preventDefault();
+    const searchUrl = "https://institucional.conasems.simoa.dev/noticias/";
+
+    try {
+      const response = await fetch(searchUrl);
+      const data = await response.json();
+      setPosts(data);
+      setSearchInput("");
+    } catch (error) {
+      console.error("Erro na busca: ", error);
+    }
+  };
+
   const handleSearchInput = async (evt) => {
     evt.preventDefault();
 
     const searchUrl = `https://institucional.conasems.simoa.dev/noticias/?q=${encodeURIComponent(
       searchInput
     )}`;
+
     try {
       const response = await fetch(searchUrl);
       const data = await response.json();
@@ -41,11 +56,19 @@ export const PostsSearchBar = () => {
           />
           <button
             type="submit"
-            className="text-white absolute end-2.5 bottom-2.5 bg-[#1A1A1A] hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2 "
+            className="text-white absolute end-5 bottom-2.5 bg-[#1A1A1A] hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2 "
           >
             Search
           </button>
         </div>
+        {searchInput.length > 0 && (
+          <button
+            className="pt-3 pl-1 underline text-white underline-offset-2 cursor-pointer"
+            onClick={handleCleanSearch}
+          >
+            Limpar pesquisa
+          </button>
+        )}
       </form>
     </>
   );
